584f4096d38c802fa433d999e2a083c5d3949a1c
Akash Bagchi <bagchi.akash120@gmail.com>
2022-02-24 21:33:17 +0530

    taking user input and console

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-console.log("i am console");
-alert("this is alert messsage");
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+
+function clickEventHandler() {
+  console.log("Clicked");
+  console.log("input:", textInput.value);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
