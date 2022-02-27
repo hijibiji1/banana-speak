202782e10ccb21deb12b43e6118622d358007df4
Akash Bagchi <bagchi.akash120@gmail.com>
2022-02-27 21:12:59 +0530

    done

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorhandler(error) {
+  console.log("error occured:", error);
+  alert("Somthing wrong with the server! Try again after some time.");
+}
+
 function clickEventHandler() {
-  outputDiv.innerText = "output is: " + textInput.value;
+  var inputText = textInput.value;
+
+  //calling server for response
+  fetch(getTranslationURL(inputText))
+    .then((Response) => Response.json())
+    .then((json) => {
+      var trasnlatedText = json.contents.translated;
+      outputDiv.innerText = trasnlatedText; // output
+    })
+    .catch(errorhandler);
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
